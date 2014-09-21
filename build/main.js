a9e1631046d14b5b606e65a91631011d36d2afc9
Simon Last <stlast1@gmail.com>
2014-09-20 19:58:28 -0700

    Add google analytics.

--- build/main.js
+++ build/main.js
@@ -774,7 +774,7 @@ React.renderComponent(
   App(null),
   document.getElementById("root")
 );
-}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_f53466ba.js","/")
+}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_20694ced.js","/")
 },{"./components/App/App.jsx":1,"1YiZ5S":26,"buffer":17}],12:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var mori = require("mori");
